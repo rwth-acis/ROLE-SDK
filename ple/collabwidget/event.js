8d7c82ec00ff8686ddb1bd15f6d7d1785e75a5be
istvank <koren@dbis.rwth-aachen.de>
2014-11-10 14:09:18 +0100

    Deleted unnecessary projects.

--- ple/collabwidget/event.js
+++
@@ -1,8 +0,0 @@
-/*global openapp*/
-
-openapp.event.connect = function(callback) {};
-openapp.event.disconnect = function() {};
-openapp.event.publish = function(envelope, message) {};
-
-//Maybe flatten RDF to namespaced-properties, like:
-//openapp.event.flattenMessage = function(message) {};
--- services/richMediaContentSearchService/src/main/java/de/imc/advancedMediaSearch/representation/search/rome/AtomNSModule.java
+++
@@ -1,9 +0,0 @@
-package de.imc.advancedMediaSearch.representation.search.rome;
-
-import com.sun.syndication.feed.module.Module;
-
-public interface AtomNSModule extends Module {
-    public static final String URI = "http://www.w3.org/2005/Atom";
-    String getLink();
-    void setLink(String href);
-}
