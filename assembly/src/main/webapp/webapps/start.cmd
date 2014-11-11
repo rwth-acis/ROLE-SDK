01ea958b2a0bfc686dee7ccbdd245d3271924df4
istvank <koren@dbis.rwth-aachen.de>
2014-11-11 17:49:49 +0100

    Added debug configuration.

--- assembly/src/main/webapp/webapps/start.cmd
+++ assembly/src/main/webapp/webapps/start.cmd
@@ -1,2 +1,2 @@
-java -Djetty.host=127.0.0.1 -Djetty.port=8073 -jar ../webapps/jetty-runner.jar --port 8073 ../webapps/role-uu-prototype --path /role ../
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5433 -Djetty.host=127.0.0.1 -Djetty.port=8073 -jar ../webapps/jetty-runner.jar --port 8073 ../webapps/role-uu-prototype --path /role ../
 pause
--- assembly/src/main/webapp/webapps/start.sh
+++ assembly/src/main/webapp/webapps/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Djetty.host=127.0.0.1 -Djetty.port=8073 -jar ../webapps/jetty-runner.jar --port 8073 ../webapps/role-uu-prototype --path /role ../
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5433 -Djetty.host=127.0.0.1 -Djetty.port=8073 -jar ../webapps/jetty-runner.jar --port 8073 ../webapps/role-uu-prototype --path /role ../
 
